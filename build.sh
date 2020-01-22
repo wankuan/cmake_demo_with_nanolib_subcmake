0a528fbe0d23d4c298f2b50a54a6a24087bfc738
wankuan-VM <794964984@qq.com>
2020-01-22 12:42:50 +0800

    delete ignore

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-ANDROID_NDK_PATH=/usr/android_SDK/ndk-bundle/
-ANDROID_NDK_LIB_PATH=/usr/android_SDK/ndk-bundle/platforms/android-20/arch-arm64/usr
+if [ ! -d build ];then
+  mkdir build
+fi
 cd ./build
 if [ "$1" = 'all' ] ;then
-    cmake\
-      -DCMAKE_BUILD_TYPE=RELEASE\
-      -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_PATH/build/cmake/android.toolchain.cmake \
-      -DANDROID_NDK=$ANDROID_NDK_PATH \
-      -DANDROID_LIB=$ANDROID_NDK_LIB_PATH \
-      -DANDROID_TOOLCHAIN=clang\
-      -DANDROID_ABI=armeabi-v7a \
-      -DANDROID_PLATFORM=android-21 \
-      -DANDROID_STL=c++_static\
-      ..
-    make
+    cmake ..
+    make -j64
 elif [ "$1" = 'clean' ];then
     make clean
     rm -rf ./*
